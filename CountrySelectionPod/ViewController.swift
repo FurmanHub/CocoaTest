b2b569d1e1b4cec082ef2496c59f0714927eaa69
Fedor Volchkov <fedor.volchkov.cr@gmail.com>
2019-10-04 16:26:38 +0300

    Initial Commit

---
+++ CountrySelectionPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountrySelectionPod
+//
+//  Created by Fedor Volchkov on 10/4/19.
+//  Copyright © 2019 Fedor Volchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
